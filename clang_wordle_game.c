165aca22ef8e8d868058ec17e925d741a69b89da
ayberkarslan <ayberkarslann0@gmail.com>
2026-02-15 22:35:22 +0300

    Taslakta kalan yorum satırındaki gereksiz kod blokları silindi, koda final formatı verildi

--- clang_wordle_game.c
+++ clang_wordle_game.c
@@ -8,18 +8,17 @@ char words[10][20] = {
     "ROKET", "YILDIZ", "TAKIM", "LINUX", "KONTROL", "AVIYONIK", "NEVSEHIR", "OTOMASYON", "MOTOR", "REKABET"
 };
 char hints[10][50] = {
-    "Gokyuzune ulasan guclu arac",          // ROKET
-    "Gece gokyuzunde parlayan kutle",       // YILDIZ
-    "Birlikte calisan insan grubu",         // TAKIM
+    "Gokyuzune ulasan guclu arac", // ROKET
+    "Gece gokyuzunde parlayan kutle", // YILDIZ
+    "Birlikte calisan insan grubu", // TAKIM
     "Acik kaynakli isletim sistemi cekirdegi", // LINUX
-    "Sistemleri yonetme ve denetleme isi",    // KONTROL
-    "Havacilik ve uzay elektroniginin ismi",        // AVIYONIK
-    "Peri bacalariyla unlu sehrimiz",       // NEVSEHIR
-    "Sistemlerin kendi kendine calismasi",   // OTOMASYON
-    "Enerjiyi harekete donusturen duzenek",  // MOTOR
-    "En iyisi olmak icin yapilan yaris"       // REKABET
+    "Sistemleri yonetme ve denetleme isi", // KONTROL
+    "Havacilik ve uzay elektroniginin ismi", // AVIYONIK
+    "Peri bacalariyla unlu sehrimiz", // NEVSEHIR
+    "Sistemlerin kendi kendine calismasi", // OTOMASYON
+    "Enerjiyi harekete donusturen duzenek", // MOTOR
+    "En iyisi olmak icin yapilan yaris" // REKABET
 };
-
 
 
 char wrongButExist[20];
@@ -32,61 +31,39 @@ int wIdx, nIdx;
 int totalTries = 0; // oyun sonunda kaç deneme ile tüm kelimeler bilindi
 
 
-
-
 void printWord(int wordNum) {
     length = strlen(words[wordNum]);
     printf("Kelime için ipuclari:\n");
     printf("Toplam %d harfli bir kelime\n", length);
     printf("%s\n", hints[wordNum]);
-    // for (; length>0; length--) {
-    //   printf("_ ");
-    //   }
 }
 
 void checkWord(int wordNum) {
-
-
-
-    //önceki tahminden kalan veriler karışmasın diye indexleri ve geçici  dizilerimizi sıfırlıyoruz
-
-
-
-
-
-
     if (strcmp(inputWord, words[wordNum]) == 0) {
-        //printf("Tebrikler! %s kelimesini dogru bildiniz!\n", words[wordNum]);
-        //bunu sildim zaten ana while içinde if koyduk
-
-    }
-    else {
+    } else {
         for (int i = 0; i < length; i++) {
             if (inputWord[i] == words[wordNum][i]) {
-              //  printf("%c", inputWord[i]); // ya da words[wordNum][i]
                 currentWord[i] = inputWord[i];
-
-            }
-            else {
-                if (strchr(words[wordNum], inputWord[i]) != NULL) { //yeri yanlış olanlar
+            } else {
+                if (strchr(words[wordNum], inputWord[i]) != NULL) {
+                    //yeri yanlış olanlar
                     // Yanlış yerdeki harf için:
-                    if (strchr(wrongButExist, inputWord[i]) == NULL) { // Eğer listede yoksa
+                    if (strchr(wrongButExist, inputWord[i]) == NULL) {
+                        // Eğer listede yoksa
                         wrongButExist[wIdx++] = inputWord[i];
                     }
-
-                }
-                else {                                              // hiç olmayanlar
+                } else {
+                    // hiç olmayanlar
                     // Olmayan harf için:
-                    if (strchr(nonExist, inputWord[i]) == NULL) { // Eğer listede yoksa
+                    if (strchr(nonExist, inputWord[i]) == NULL) {
+                        // Eğer listede yoksa
                         nonExist[nIdx++] = inputWord[i];
                     }
                 }
             }
         }
     }
-
 }
-
 
 
 void printStatus(int j) {
@@ -109,9 +86,6 @@ void printStatus(int j) {
 }
 
 
-
-
-
 int main(void) {
     printf("====================================================\n");
     printf("\t\t           --Wordle Game--\n");
@@ -119,23 +93,23 @@ int main(void) {
     printf("Not:Türkce karakterler (İ,ı,ş,ç gibi) ve küçük harfler kullanmayınız.\n");
 
 
+    wIdx = 0, nIdx = 0; // bunları kelime değişmeden önce bir kere sıfırlayacağız.
 
-
-    wIdx=0 , nIdx = 0;// bunları kelime değişmeden önce bir kere sıfırlayacağız.
-
-    for (int n=0; n<10 ; n++) {
+    for (int n = 0; n < 10; n++) {
         length = strlen(words[n]);
         int tryCount = 0; //kelimeyi bilince istatistik paneli eklemek için, her kelimede sıfırlansın
-        for(int i = 0; i < length; i++) currentWord[i] = '_'; //başta "_ _ _ _ _" şeklinde kelime çıktısını verelim, zamanla dolsun.
+        for (int i = 0; i < length; i++) currentWord[i] = '_';
+        //başta "_ _ _ _ _" şeklinde kelime çıktısını verelim, zamanla dolsun.
         currentWord[length] = '\0'; // string sonu işareti
 
 
         // listeleri ve sayaçları sıfırladık
-        wIdx = 0; nIdx = 0;
+        wIdx = 0;
+        nIdx = 0;
         memset(wrongButExist, 0, sizeof(wrongButExist));
         memset(nonExist, 0, sizeof(nonExist));
 
-        while (1){
+        while (1) {
             tryCount++;
             printWord(n);
             printf("\n\t\t>>> ");
@@ -143,10 +117,12 @@ int main(void) {
             checkWord(n);
 
 
-            if (strcmp(inputWord, words[n]) == 0) { // eğer kelime doğruysa status basmayıp direkt döngüden çıktık
+            if (strcmp(inputWord, words[n]) == 0) {
+                // eğer kelime doğruysa status basmayıp direkt döngüden çıktık
                 totalTries += tryCount; //bu kelime için denemeleri toplam denemelere ekleyelim
                 system("clear"); //ekran temiz dursun diye
-                printf("\n\033[0;32mCevabiniz dogru! %s kelimesini %d denemede bildiniz.\n\033[0m\n",words[n], tryCount);
+                printf("\n\033[0;32mCevabiniz dogru! %s kelimesini %d denemede bildiniz.\n\033[0m\n", words[n],
+                       tryCount);
                 printf("Bir sonraki kelimeye geciyoruz...\n-----------------------------\n");
                 break;
             }
@@ -169,13 +145,13 @@ int main(void) {
     printf("            OYUN BITTI - TEBRIKLER!                 \n");
     printf("====================================================\n");
     printf(" Toplam Kelime Sayisi  : 10                         \n");
-    printf(" Toplam Tahmin Sayisi  : %d                         \n",totalTries);
-    float success= (10.0/totalTries)*100; //başarı oranı da hesapladık yüzde olarak - 10.0 yazdık çünkü tam sayı/tam sayı olursa gcc derleyicisi virgulden sonrasını atar
-    printf(" Basari yuzdeniz       : %.1f                         \n", success); // %% kullanarak yüzdeyi, .1f ile küsuratı ayarladık
+    printf(" Toplam Tahmin Sayisi  : %d                         \n", totalTries);
+    float success = (10.0 / totalTries) * 100;
+    //başarı oranı da hesapladık yüzde olarak - 10.0 yazdık çünkü tam sayı/tam sayı olursa gcc derleyicisi virgulden sonrasını atar
+    printf(" Basari yuzdeniz       : %.1f                         \n", success);
+    // %% kullanarak yüzdeyi, .1f ile küsuratı ayarladık
     printf("====================================================\n");
     printf("\033[0m\n");
-
-   // printf("Döngüden çıktın.\n");
 
     return 0;
 }
